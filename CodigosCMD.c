1ad516c655b2fd19389481e1df79124bbe0a4b2c
Facundo Manuel Minatto <facuminatto@gmail.com>
2025-12-23 10:55:22 -0300

    Mega Update
    Se hizo codigo de nuevo, con mas conocimiento en lenguaje C.
    Se debe ejecutar si o si como administrador para que funciones correctamente.

--- CodigosCMD.c
+++ CodigosCMD.c
@@ -1,64 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int var;
+void imprimir(void);
+int ingresar(void);
 
-int main() {
-    printf("    \n¿Qué comando para CMD necesitas?\n\n");
-    printf("    |1| Activador de Windows - Office\n");
-    printf("    |2| Reiniciar ajustes de red\n");
-    printf("    |3| Borrar cola de la impresora\n");
-    printf("    |4| Actualizar aplicaciones de terceros\n");
-    printf("    |5| Escanear el disco en busca de errores\n\n");
+void activator (void);
+void red (void);
+void imp (void);
+void disk (void);
 
-    // Pedir opcion valida
-    do {
-        
-        scanf("%d", &var);
-
-        if (var < 1 || var > 5) 
-            printf("Ingrese un numero valido.\n");
-
-    } while (var < 1 || var > 5); // Acepta solo entre 1 y 5 inclusive
-
-    switch (var) {
-        case 1:
-			printf("\n(En powershell)\n");
-            printf("\nirm https://get.activated.win | iex \n");
-            break;
-
-        case 2:
-			printf("\nnetsh winsock reset");
-			printf("\nnetsh int ip reset");
-			printf("\nipconfig /release");
-			printf("\nipconfig /renew");
-			printf("\nipconfig /flushdns");
-			printf("\nipconfig /registerdns");
-            break;
-
-        case 3:
-            printf("\nnet stop spooler\n");
-			printf("\n(Busca la carpeta system32 - spool - printers).\n");
-			printf("\n(Borra lo que haya dentro).\n");
-			printf("\nnet start spooler\n");
-            break;
-
-        case 4:
-            printf("\n(En powershell)\n");
-			printf("\nwinget update");
-			printf("\nWINGET UPDATE --ALL");
-            break;
-
-        case 5:
-			printf("\nDISM /online /cleanup-image /checkhealth\n");
-			printf("DISM /online /cleanup-image /scanhealth\n");
-			printf("dism /online /Cleanup-Image /RestoreHealth\n");
-			printf("sfc /scannow\n");
-            break;
-    }
-
-	printf("\nPresiona Enter para salir...");
-	getchar();  // Consumir el '\n' pendiente de scanf
-	getchar(); 
+int main(void) {
 	
-    return 0;
+	void (*p[5])(void) = {activator, red, imp, disk};
+	
+	imprimir();
+	int valor = ingresar();
+	
+	p[valor - 1]();
+	
+	return 0;
+}
+
+void imprimir(void){
+	
+	printf("Â¿QuÃ© comando para CMD necesitas?\n\n");
+	
+	printf("|1| Activador de Windows - Office\n");
+	printf("|2| Reiniciar ajustes de red\n");
+	printf("|3| Borrar cola de la impresora\n");
+	printf("|4| Escanear el disco en busca de errores\n");
+	printf("|0| Salir\n\n");
+}
+	
+int ingresar(void) {
+	
+	int var;
+	
+	do {
+		printf("Ingrese un numero: ");
+		scanf("%d", &var);
+		
+		if (var == 0)
+			exit(1);
+		
+	} while(var < 0 || var > 4);
+	
+	return var;
+}
+
+void esperar_enter(void) {
+	
+	printf("\nPresione ENTER para continuar al siguiente paso...");
+
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF) { }
+
+	getchar(); 
+}
+
+void activator (void){
+	
+	system("powershell -Command \"irm https://get.activated.win | iex\"");
+	
+}
+
+void red (void){
+	
+	system("netsh winsock reset");
+	
+	system("netsh int ip reset");
+	
+	system("ipconfig /release");
+	
+	system("ipconfig /renew");
+	
+	system("ipconfig /flushdns");
+	
+	system("ipconfig /registerdns");
+}
+
+void imp (void){
+	
+	system("net stop spooler");
+	
+	printf("\nBuscar en system32 la carpeta spool - printers; Y borras todo.\n");
+	esperar_enter();
+	
+	system("net start spooler");
+}
+
+void disk (void){
+	
+	system("DISM /online /cleanup-image /checkhealth");
+	system("DISM /online /cleanup-image /scanhealth");
+	system("dism /online /Cleanup-Image /RestoreHealth");
+	system("sfc /scannow");
+	
 }
